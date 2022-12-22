a347d791941073915fd4fdd51eb9b74152b53c74
chasebarn35 <chasebarn35@tamu.edu>
2022-12-22 05:07:10 -0600

    worked on some of the addition

--- common.h
+++ common.h
@@ -8,19 +8,36 @@
 class long_256 {
 	private:
 		long* data;
-		int size;
+		int _size;//maybe look into changing to a size_t?
 	public:
-	long_256(std::initializer_list<long> list): data(new long[list.size()]), size(list.size()) {
+	long_256(int __size): data(new long[__size]),_size(__size){}	
+	long_256(std::initializer_list<long> list): data(new long[list.size()]), _size(list.size()) {
 		for (auto i : list){
 			(*data) = i;
 			data++;
 		}
-		data-=size;
-		for (int i = 0; i < size; ++i)
+		data-=_size;
+		for (int i = 0; i < _size; ++i)
 			std::cout << data[i];
 	}
+
 	~long_256(){delete[] data;}
+	
+	int size(){return _size;}
+
+	long_256 operator+ (long_256 other){
+	int maxVal = (_size > other.size())? _size:other.size();
+	long_256 newNum(maxVal);//assume zero'd out. Ill get that working at some point
+	for(int i =0; i < _size; ++i){
+		
+
+	}
+
+	return newNum;
+	}
+
 	long_256 operator* (long_256 other);//TODO
+	
 
 };
 

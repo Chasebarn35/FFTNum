98b41c697c3e8622f93c4a6f6bc91bef02856cd9
chasebarn35 <chasebarn35@tamu.edu>
2022-12-22 21:10:35 -0600

    operator<<, parts of operator+

--- common.h
+++ common.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <initializer_list>
 #include <climits>
+#include <string>
+
 
 class long_256 {
 	private:
@@ -17,28 +19,48 @@ class long_256 {
 			data++;
 		}
 		data-=_size;
-		for (int i = 0; i < _size; ++i)
-			std::cout << data[i];
+	}
+
+	long& operator[](int offset) const noexcept{
+		return data[offset];
 	}
 
 	~long_256(){delete[] data;}
 	
-	int size(){return _size;}
+	int size() const noexcept {return _size;}
 
 	long_256 operator+ (long_256 other){
 	int maxVal = (_size > other.size())? _size:other.size();
-	long_256 newNum(maxVal);//assume zero'd out. Ill get that working at some point
-	for(int i =0; i < _size; ++i){
-		
-
+	long_256 newNum(maxVal+1);//assume zero'd out. Ill get that working at some point
+	for(int i = 0; i < maxVal; ++i){
+		if(LONG_MAX - data[i] > other[i]){//TODO CHECKS FOR MAKING SURE THE NUMBERS LINE UP
+			newNum[i+1] = 1;
+			newNum[i] = LONG_MIN+data[i]+other[i] + newNum[i];
+		}
+		else
+			newNum[i] = newNum[i] + data[i]+other[i];
 	}
 
 	return newNum;
 	}
+	
 
 	long_256 operator* (long_256 other);//TODO
 	
 
 };
+	
+
+std::ostream& operator<<(std::ostream& os,const long_256& l256){
+	os << std::to_string(l256[0]);//No need for zero extension
+	std::string zeroExtension,newLong;
+	for(int i = 1; i < l256.size();++i){
+		newLong = std::to_string(l256[i]);
+		zeroExtension.clear();
+		zeroExtension.append(19-newLong.length(),'0');
+		os << zeroExtension + newLong;
+	}
+	return os;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@
 int main(){
 
 	long_256 test1({123,LONG_MAX,789,LONG_MAX,LONG_MAX,LONG_MAX});
+	
+	long_256 test2({1,2,3});
+	std::cout << test1 << std::endl;
+	std::cout << test2 << std::endl;
 
 }
